2ec9a13a1fadd13f3608c84d89b9a592bf8773e9
ʄʀǟռᏦɛռֆ⊺ɛɨռ™ ❬❬ઝ¡ทg_૨¡ρ⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-12-13 23:15:29 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -165,7 +165,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             send_start(bot, update)
     else:
         update.effective_message.reply_text("Heya,{} Here..\nHow can I help you? 💃".format(bot.first_name),reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))]]))
+                                                [[InlineKeyboardButton(text="Help❓",url="t.me/{}?start=help".format(bot.username))]]))
 
 def send_start(bot, update):
     #Try to remove old message
@@ -179,7 +179,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Help ❓",callback_data="help_back"),InlineKeyboardButton(text="Creator 🔱",url="https://t.me/DON_OF_TELEGRAM")]]
+    keyboard = [[InlineKeyboardButton(text="Help ",callback_data="help_back"),InlineKeyboardButton(text="Creator 🔱",url="https://t.me/DON_OF_TELEGRAM")]]
     keyboard += [[InlineKeyboardButton(text="Connect Group 🔄", callback_data="main_connect"),InlineKeyboardButton(text="Add Me 🔆",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
@@ -280,8 +280,8 @@ def get_help(bot: Bot, update: Update):
 
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="❓Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🧞‍♂Contact Creator",url="https://t.me/DON_OF_TELEGRAM")]]))
+                                                [[InlineKeyboardButton(text="Help❓",url="t.me/{}?start=help".format(bot.username))],  
+                                                [InlineKeyboardButton(text="Contact Creator 🚸",url="https://t.me/DON_OF_TELEGRAM")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
