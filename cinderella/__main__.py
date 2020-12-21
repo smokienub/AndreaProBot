eaf8394483dd7e41b201b99ba28b7b2a5c31cdc0
ʄʀǟռᏦɛռֆ⊺ɛɨռ™ ❬❬ઝ¡ทg_૨¡ρ⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2020-12-21 20:37:12 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -58,8 +58,8 @@ def vercheck() -> str:
 
 
 SOURCE_STRING = """
-⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://github.com/DONOFTELEGRAM/AndreaProBot)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://github.com/DONOFTELEGRAM/Andreaprobot.git)
+⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource()
+⚡You Can Clone Me [Here](https://t.me/DON_OF_TELEGRAM)
 """
 
 
