eaa6e92bec32e869f1897c98dfff481c155dc0da
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-02-21 10:51:30 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -235,7 +235,7 @@ def help_button(bot: Bot, update: Update):
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
                                      reply_markup=InlineKeyboardMarkup(
-                                         [[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]))
+                                         [[InlineKeyboardButton(text="🔙 Back", callback_data="help_back")]]))
 
         elif prev_match:
             curr_page = int(prev_match.group(1))
@@ -288,7 +288,7 @@ def get_help(bot: Bot, update: Update):
         module = args[1].lower()
         text = "Here is the available help for the *{}* module:\n".format(HELPABLE[module].__mod_name__) \
                + HELPABLE[module].__help__
-        send_help(chat.id, text, InlineKeyboardMarkup([[InlineKeyboardButton(text="🔙Back", callback_data="help_back")]]))
+        send_help(chat.id, text, InlineKeyboardMarkup([[InlineKeyboardButton(text="🔙 Back", callback_data="help_back")]]))
 
     else:
         send_help(chat.id, HELP_STRINGS)
