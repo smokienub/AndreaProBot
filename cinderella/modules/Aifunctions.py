b409338c948c8b70d6897aa1667c12756b46d2dc
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-03-27 08:51:58 +0530

    New ai module by @mr_srlock

--- cinderella/modules/Aifunctions.py
+++ cinderella/modules/Aifunctions.py
@@ -1,16 +1,19 @@
-# 🙈 Give me ⭐️ before kanging code
-# © @Mr_Dark_Prince 🌚
+# 🙈 Give me ⭐️ before kanging code.
+# kanged from @Mr_Dark_Prince 🌚.
+# installed by @mr_srlock.🙊
+# All credits belongs to @mr_dark_prince without his efforts this wrok is undone.
+# frist time in a cinderella version.introduced by ai intelligence module by @mr_srlock.
 
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from MashaRoBot import telethn, OWNER_ID
-from MashaRoBot.events import register
+from cinderella import client, OWNER_ID
+from cinderella.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
 import asyncio, os
 
-@register(pattern="Masha (.*)")
+@register(pattern="Andrea (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -29,24 +32,24 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "I am fairly yound and I was made by @Mr_Dark_Prince."
+   pro = "I am fairly yound and Im here by @mr_srlock."
    try:
-      async with telethn.action(event.chat_id, 'typing'):
+      async with client.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
            await event.reply(pro)
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Masha"
+   pro = "My name is Andrea"
    try:
-      async with telethn.action(event.chat_id, 'typing'):
+      async with client.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
            await event.reply(pro)
    except CFError as e:
            print(e)
   else:
     try:
-      async with telethn.action(event.chat_id, 'typing'):
+      async with client.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
            await event.reply(result)
     except CFError as e:
